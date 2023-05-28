01919671df18cb34297ee3aa26925a452717996c
Nekitosn <nikitasedov57@gmail.com>
2023-05-28 12:51:46 +0300

    feat - Created entities; feat - Created migration.

---
+++ MoviePlex.Infrastructure/Entities/CinemaHall.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviePlex.Infrastructure.Entities;
+
+public class CinemaHall
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CountOfSeats { get; set; }
+    public decimal Price { get; set; }
+
+    public List<Session> Sessions { get; set; }
+}
---
+++ MoviePlex.Infrastructure/Entities/Enum/GenreFilm.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviePlex.Infrastructure.Entities.Enum;
+
+public enum  GenreFilm
+{
+    Comedy = 1,
+    Fantasy = 2,
+    Drama = 3,
+    History = 4,
+    Horror = 5,
+    
+    Other = 99
+}
