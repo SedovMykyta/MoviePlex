185b7b9eca6494ddf25387d48ea37155bdfef82c
Nekitosn <nikitasedov57@gmail.com>
2023-06-09 16:15:37 +0300

    feat - Created home controller; feat - Created film controller; feat - Created film view: adminPanel, create, delete, edit, getListAdmin, index and partial view: inputForm; feat - Created home view: index; feat - Rework _layout and _errorPartial; feat - Added temporary bootswatch style; feat - Implemented all dependency in Program.cs; fix - Deleted folder models and views: _validationScriptsPartial, error.

---
+++ MoviePlex.UI/Controllers/FilmController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviePlex.Core.Areas.Films;
+using MoviePlex.Core.Areas.Films.Dtos;
+using MoviePlex.Core.Areas.Validators;
+using MoviePlex.UI.Utilities;
+
+namespace MoviePlex.UI.Controllers;
+
+public class FilmController : Controller
+{
+    private readonly IFilmService _filmService;
+    private readonly IValidationService _validationService;
+    
+    public FilmController(IFilmService filmService, IValidationService validationService)
+    {
+        _filmService = filmService;
+        _validationService = validationService;
+    }
+    
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public ActionResult AdminPanel()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetListAdminAsync()
+    {
+        var films = await _filmService.GetListAsync();
+        
+        return View(films);
+    }
+    
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> CreateAsync(FilmInputDto filmInput)
+    {
+        var result = await _validationService.ValidateAsync(filmInput);
+        if (! result.IsValid)
+        {
+            result.AddToModelState(ModelState);
+            
+            return View(filmInput);
+        }
+        
+        await _filmService.CreateAsync(filmInput);
+        
+        return RedirectToAction("GetListAdmin");
+    }
+    
+    public async Task<ActionResult> EditAsync(int id)
+    {
+        var film = await _filmService.GetInputDtoByIdAsync(id);
+        
+        return View(film);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> EditAsync(int id, FilmInputDto filmInput)
+    {
+        var result = await _validationService.ValidateAsync(filmInput);
+        if (! result.IsValid)
+        {
+            result.AddToModelState(ModelState);
+            
+            return View(filmInput);
+        }
+        
+        await _filmService.UpdateByIdAsync(id, filmInput);
+        
+        return RedirectToAction("GetListAdmin");
+    }
+    
+    [HttpDelete]
+    public async Task<ActionResult> DeleteAsync(int id)
+    {
+        await _filmService.DeleteByIdAsync(id);
+        
+        return RedirectToAction("GetListAdmin");
+    }
+}
